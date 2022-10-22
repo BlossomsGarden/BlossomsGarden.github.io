5f537d42850ec4cf76e90596db28024e07ffc05a
BlossomsGarden <1419234114@qq.com>
2022-10-22 16:49:28 +0800

    小改
    header.html和header.css找不到

--- HTMLhomework/my_javascript/load_header.js
+++ HTMLhomework/my_javascript/load_header.js
@@ -1,3 +1,5 @@
 $(function(){
+    /*github上要加上大文件夹的名字，否则找不对文件*/
     $("#header").load("./HTMLhomework/my_common_unit/header.html");
+    /*$("#header").load("./my_common_unit/header.html");*/
 });
